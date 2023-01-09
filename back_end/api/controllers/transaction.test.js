257d3f4c920df9307a327cd5dd3555d3e9c1d3fd
John Sciutto <johnsciutto@gmail.com>
2023-01-08 20:47:15 -0500

    Add skeleton for transaction tests

---
+++ back_end/api/controllers/transaction.test.js
@@ -0,0 +1,23 @@
+const { describe, test, expect } = require('@jest/globals');
+
+describe('transaction controller', () => {
+  describe('addTransaction', () => {
+    test.todo('...')
+  })
+
+  describe('editTransaction', () => {
+    test.todo('...')
+  })
+
+  describe('deleteTransaction', () => {
+    test.todo('...')
+  })
+
+  describe('getTransaction', () => {
+    test.todo('...')
+  })
+
+  describe('getTransactions', () => {
+    test.todo('...')
+  })
+})
