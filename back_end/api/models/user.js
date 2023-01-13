c4dc28caca6af25ca39fc0300c213d328cb8e392
John Sciutto <johnsciutto@gmail.com>
2023-01-13 07:06:13 -0500

    Make the user model paranoid (soft deletion)

--- back_end/api/models/user.js
+++ back_end/api/models/user.js
@@ -1,24 +1,30 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../../config/db');
 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const User = sequelize.define(
+  'User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    paranoid: true,
+  }
+);
 
 module.exports = User;
