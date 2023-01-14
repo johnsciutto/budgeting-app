6191efd4d7078ba40def4ab6fc2064f16a984820
John Sciutto <johnsciutto@gmail.com>
2023-01-14 16:22:36 -0500

    Add routers to main app

--- back_end/api/index.js
+++ back_end/api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { userRouter } = require('./routes');
+const { userRouter, categoryRouter, transactionRouter } = require('./routes');
 
 const router = express.Router();
 
 router.use('/user', userRouter);
+router.use('/category', categoryRouter);
+router.use('/transaction', transactionRouter);
 
 module.exports = router;
