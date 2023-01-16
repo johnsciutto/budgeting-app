a47fe2ec1564fbe7d7b8aae4b9ea5b9d9eb8709f
John Sciutto <johnsciutto@gmail.com>
2023-01-15 20:47:26 -0500

    Write first tests for protectRoute middleware

--- back_end/api/controllers/middleware/auth.test.js
+++ back_end/api/controllers/middleware/auth.test.js
@@ -1,10 +1,37 @@
 const { describe, test, expect } = require('@jest/globals');
+const { protectRoute } = require('./auth');
 
 describe('protectRoute', () => {
-  test.todo('should return an error if the Authorization header is missing');
+  const req = { headers: {} };
+  const res = {
+    statusCode: null,
+    message: null,
+    headers: {},
+    status: function (num) {
+      this.statusCode = num;
+      return this;
+    },
+    send: function (message) {
+      this.message = message;
+      return true;
+    },
+  };
+  const next = () => true;
 
-  test.todo('should return an error if the Bearer token is missing');
+  test('should return an error if the Authorization header is missing', () => {
+    protectRoute(req, res, next);
+    expect(res.statusCode).toBe(401);
+    expect(res.message).toBe('Authorization header is missing');
+  });
 
+  test('should return an error if the Bearer token is missing', () => {
+    req.headers['authorization'] = 'somethingthatisnotvalid';
+    protectRoute(req, res, next);
+    expect(res.statusCode).toBe(401);
+    expect(res.message).toBe('Bearer token is missing');
+  });
+
+  // TODO: This is next...
   test.todo(
     "should return an error if the decoded token doesn't have the necessary properties"
   );
@@ -14,4 +41,12 @@ describe('protectRoute', () => {
   );
 
   test.todo('should return an error if the token expired');
+
+  afterEach(() => {
+    res.statusCode = null;
+    res.message = null;
+    res.headers = {};
+
+    req.headers = {};
+  });
 });
