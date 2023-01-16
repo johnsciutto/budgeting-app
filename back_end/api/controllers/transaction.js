1eca94169fb7bf6ec2ba8275cdbef52dea704f72
John Sciutto <johnsciutto@gmail.com>
2023-01-16 12:58:24 -0500

    Delete leftover TODO

--- back_end/api/controllers/transaction.js
+++ back_end/api/controllers/transaction.js
@@ -44,7 +44,6 @@ const addTransaction = async (req, res) => {
 const editTransaction = async (req, res) => {
   const response = { ok: true, error: null };
   try {
-    // NOTE: Add security to these routes (make sure that only the owner of the transactions can edit)
     const userId = req.user?.id;
     const { transactionId } = req.params;
     const { date, name, amount, type, category, note } = req.body;
