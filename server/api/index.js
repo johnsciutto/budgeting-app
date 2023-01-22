56d70f8d9d55dacf8c025f154789ca5e1f631413
John Sciutto <johnsciutto@gmail.com>
2023-01-22 14:10:43 -0500

    Add body-parser

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const { userRouter, categoryRouter, transactionRouter } = require('./routes');
 
 const router = express.Router();
+
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: true }))
 
 router.use('/user', userRouter);
 router.use('/category', categoryRouter);
